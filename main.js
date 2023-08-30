1109ceed15ccde1e6837b9b8e2efff2f321d2e99
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2023-08-30 12:55:04 +0530

    added files

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: [" Learning is my Passion", "CP Enthusiast", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var typ = new Typed(".tet",{
+    strings: ["Uthrisar Khungur Boro"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+})
