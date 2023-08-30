1d3aa4ce1a8fa7f75907d0ecdcb0434c6ba242ee
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2023-08-31 03:51:25 +0530

    Update main.js
    added

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 var typed = new Typed(".text", {
-    strings: [" Learning is my Passion", "CP Enthusiast", "Web Developer"],
+    strings: [" exploring....", "a CP Enthusiast.", " a Web Developer."],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
 
-var typ = new Typed(".tet",{
+var my_name = new Typed(".tet",{
     strings: ["Uthrisar Khungur Boro"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 500,
     loop: true
 })
+
+var about_me = new Typed(".tet2", {
+    strings: ["I am a skilled web designer with over 2 years of experience, Eager to dive deep into the Machine Learning, love to think roots and problems of Mathematics, want to solve Real World Problem to various field using technology."],
+    typeSpeed: 20,
+    backSpeed: 10,
+    backDelay: 2000,
+    loop: true
+})
+
