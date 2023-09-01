dc0acab564371d1b2419849253cba4f0e757eeba
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2023-09-01 12:51:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var typed = new Typed(".text", {
-    strings: [" exploring....", "a CP Enthusiast.", " a Web Developer."],
-    typeSpeed: 100,
-    backSpeed: 100,
+var my_name = new Typed(".text1",{
+    strings: ["Mathematics and Computing", "At IIT Guwahati."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 800,
+    loop: true
+})
+var typed = new Typed(".text2", {
+    strings: ["Coder.", "a CP Enthusiast.", " a Web Developer."],
+    typeSpeed: 80,
+    backSpeed: 80,
     backDelay: 1000,
     loop: true
 });
 
-var my_name = new Typed(".tet",{
-    strings: ["Uthrisar Kh Boro"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 500,
-    loop: true
-})
 
-var about_me = new Typed(".tet2", {
-    strings: ["I am a skilled web designer with over 2 years of experience, Eager to dive deep into the Machine Learning, love to think roots and problems of Mathematics, want to solve Real World Problem to various field using technology."],
-    typeSpeed: 20,
-    backSpeed: 10,
-    backDelay: 2000,
+var about_me_1 = new Typed(".text3", {
+    strings: ["I am a skilled web designer with over 2 years of experience, eager to dive deep into the Machine Learning, love to think roots and problems of Mathematics, want to solve Real World Problem to various field using technology."],
+    typeSpeed: 15,
+    backSpeed: 5,
+    backDelay: 1000,
     loop: true
 })
 
