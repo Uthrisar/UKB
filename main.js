3e79263acbc3dbff9ac92d41f80359b8c2d7a363
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2023-10-09 00:49:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var my_name = new Typed(".text1",{
+    strings: ["Mathematics and Computing", "At IIT Guwahati."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 800,
+    loop: true
+})
+var typed = new Typed(".text2", {
+    strings: ["Coder.", "a CP Enthusiast.", " a Web Developer."],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true
+});
+
+
+var about_me_1 = new Typed(".text3", {
+    strings: ["I am a skilled web designer with over 2 years of experience, eager to dive deep into the Machine Learning, love to think roots and problems of Mathematics, want to solve Real World Problem to various field using technology."],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 100,
+    loop: true
+})
+
+let prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector(".header").classList.remove("hide-navbar");
+    } else {
+        document.querySelector(".header").classList.add("hide-navbar");
+    }
+    
+    prevScrollPos = currentScrollPos;
+};
